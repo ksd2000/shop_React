a60268d9915d3a150b763080f7255e46b3dab5db
ksd2000 <ksd2000@yandex.ru>
2020-10-06 21:52:08 +0300

    Updates

---
+++ precache-manifest.7229cb478aeae6b61e368832b5e4c6d7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bef800838e9eb36ec9d994cc557860",
+    "url": "/shop_React/index.html"
+  },
+  {
+    "revision": "a10d3035a714659e9723",
+    "url": "/shop_React/static/css/main.d1b3c497.chunk.css"
+  },
+  {
+    "revision": "b52da376a7f885dc138c",
+    "url": "/shop_React/static/js/2.bed3fbec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop_React/static/js/2.bed3fbec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10d3035a714659e9723",
+    "url": "/shop_React/static/js/main.09fcb4d8.chunk.js"
+  },
+  {
+    "revision": "f15ff0447dc8b186eb71",
+    "url": "/shop_React/static/js/runtime-main.2147d0a8.js"
+  },
+  {
+    "revision": "8df842a7e80f18fa8535a3041ccada68",
+    "url": "/shop_React/static/media/1.8df842a7.png"
+  },
+  {
+    "revision": "ff45f81f50e03e619018c0ecb6c79a93",
+    "url": "/shop_React/static/media/10.ff45f81f.png"
+  },
+  {
+    "revision": "4f7cf4642e9f1f2a790ee293f4b97283",
+    "url": "/shop_React/static/media/2.4f7cf464.png"
+  },
+  {
+    "revision": "7980fdf13adb83e0b584d33a5b5999a1",
+    "url": "/shop_React/static/media/3.7980fdf1.png"
+  },
+  {
+    "revision": "8df93ba479994b7b6d7fc3717f0a50ef",
+    "url": "/shop_React/static/media/4.8df93ba4.png"
+  },
+  {
+    "revision": "dd3b764a3f48aa80404c44aae81fa456",
+    "url": "/shop_React/static/media/5.dd3b764a.png"
+  },
+  {
+    "revision": "0f8711813c815e769e1caa7d10975305",
+    "url": "/shop_React/static/media/6.0f871181.png"
+  },
+  {
+    "revision": "32c9d97b59eac56735ff22655fa610fa",
+    "url": "/shop_React/static/media/7.32c9d97b.png"
+  },
+  {
+    "revision": "35945189cbc76f4410a3d91358def755",
+    "url": "/shop_React/static/media/8.35945189.png"
+  },
+  {
+    "revision": "97305768eb7aee5ae37fc5410043be43",
+    "url": "/shop_React/static/media/9.97305768.png"
+  }
+]);
